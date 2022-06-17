13f62736daa1e00db03362420679c19ce0f17814
leedo97y <dlehdus1433@gmail.com>
2022-06-18 01:57:16 +0900

    add fetch in node

--- React/advence/API fetch.js
+++ React/advence/API fetch.js
@@ -1,3 +1,4 @@
+import fetch from "node-fetch";
 // API : 서버에서 데이터를 요청하고, 데이터를 가져오고 그 데이터를 클라이언트에게 줌
 // = 서버와 프로그램 간의 연결, 다리 정도
 /**
@@ -6,8 +7,9 @@
  * => 비동기 호출 한다.
  */
 
+
 // [ fetch ]
-let response = fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
+let response = fetch('https://jsonplaceholder.typicode.com/posts').then((res) =>
   console.log(res)
 );
 // : 자바스크립트에서 API를 호출할 수 있게 도와주는 내장함수 / Promise = 비동기처리 , then 사용
@@ -16,9 +18,19 @@ let response = fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
 // ___________________________________________________________________________________________
 
 async function getData() {
-  let rawResponse = await fetch("https://jsonplaceholder.typicode.com/posts");
+  let rawResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
   let jsonResponse = await rawResponse.json();
   console.log(jsonResponse);
 }
 
 getData();
+// => 오류 : fetch가 사용이 안됨
+
+
+async function aA() {
+  let rep = await fetch('https://jsonplaceholder.typicode.com/posts');
+  let jsonRe = await rep.json();
+  console.log(jsonRe);
+}
+
+aA();
